32e2ee9fee2414e65a9e62622bdbc6df2b1b7eed
Chris Ra <chraab@gmail.com>
2022-07-12 12:51:52 +0200

    models.py: Improve doc strings.

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,28 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+
+    :param data: Inflammation values for M days as int array of shape (N, M)
+    :returns: Daily mean, float array of size M
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+
+    :param data: Inflammation values for M days as int array of shape (N, M)
+    :returns: Daily maximum, int array of size M
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+
+    :param data: Inflammation values for M days as int array of shape (N, M)
+    :returns: Daily minimum, int array of size M
+    """
     return np.min(data, axis=0)
 
