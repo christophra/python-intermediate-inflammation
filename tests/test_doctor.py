89e431dbab3625f5eb00ab36f1c8bd81ae6207c5
Chris Ra <chraab@gmail.com>
2022-07-15 12:09:49 +0200

    Split test_models into more files

---
+++ tests/test_doctor.py
@@ -0,0 +1,42 @@
+"""Tests for the Doctor model."""
+
+import numpy as np
+import numpy.testing as npt
+import pytest
+
+
+def test_create_doctor():
+    """Test Doctor class constructor"""
+    from inflammation.models import Doctor
+    name = 'JD'
+    doc = Doctor(name)
+    assert doc.name == name
+    assert isinstance()
+
+
+def test_doctor_is_person():
+    """Test if a doctor is a person"""
+    from inflammation.models import Doctor, Person
+    doc = Doctor("Elliot Reid")
+    assert isinstance(doc, Person)
+
+
+def test_patients_added_correctly():
+    """Check patients are being added correctly by a doctor. """
+    from inflammation.models import Doctor, Patient
+    doc = Doctor("Sheila Wheels")
+    alice = Patient("Alice")
+    doc.add_patient(alice)
+    assert doc.patients is not None
+    assert len(doc.patients) == 1
+    assert doc["Alice"] is not None
+
+
+def test_no_duplicate_patients():
+    """Check adding the same patient to the same doctor twice does not result in duplicates. """
+    from inflammation.models import Doctor, Patient
+    doc = Doctor("Sheila Wheels")
+    alice = Patient("Alice")
+    doc.add_patient(alice)
+    doc.add_patient(alice)
+    assert len(doc.patients) == 1
--- tests/test_patient.py
+++ tests/test_patient.py
@@ -1,10 +1,20 @@
 """Tests for the Patient model."""
 
+import numpy as np
+import numpy.testing as npt
+import pytest
 
-def test_create_patient():
-    from inflammation.models import Patient
 
-    name = 'Alice'
-    p = Patient(name=name)
+def test_patient():
+    """Test Patient class constructor."""
+    from inflammation.models import Patient, Person
 
-    assert p.name == name
+    alice = Patient('Alice')
+    assert str(alice) == 'Alice'
+
+    assert alice.last_observation is None
+    alice.add_observation(42)
+    assert alice.last_observation.value == 42
+    alice.add_observation(32)
+    assert alice.last_observation.value == 32
+    assert isinstance(alice, Person)
